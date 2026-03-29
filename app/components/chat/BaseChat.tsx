aa9de8cdde086f8c5bca0cee050a22b3da12727b
diyorbe-beep <marufzonkodirov0@gmail.com>
2026-03-29 22:23:47 +0500

    ad

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -7,7 +7,6 @@ import React, { type RefCallback, useEffect, useState } from 'react';
 import { ClientOnly } from 'remix-utils/client-only';
 import { Menu } from '~/components/sidebar/Menu.client';
 import { Workbench } from '~/components/workbench/Workbench.client';
-import { workbenchStore } from '~/lib/stores/workbench';
 import { classNames } from '~/utils/classNames';
 import { PROVIDER_LIST } from '~/utils/constants';
 import { Messages } from './Messages.client';
